83436b38d9b9d956d772ccea504f983f449a2aa4
Jay Yoon <lee.jayyoon@gmail.com>
2017-10-23 09:57:28 -0400

    added python command on *.sh files

--- collect_twitter_dialogs/collect.sh
+++ collect_twitter_dialogs/collect.sh
@@ -2,4 +2,4 @@
 
 target=account_names_for_dstc6.txt
 datetime=`date +"%Y-%m-%d_%H-%M-%S"`
-collect_twitter_dialogs.py -t $target -o ./stored_data -l ./collect_${datetime}.log 
+python collect_twitter_dialogs.py -t $target -o ./stored_data -l ./collect_${datetime}.log 
